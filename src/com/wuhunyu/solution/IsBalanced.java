2480ed50bb9e938ed3a43735107c4590d7804922
gongzhiqiang <yytgzq@yymt.com>
2022-08-17 13:42:11 +0800

    feat(题解): 新增 110. 平衡二叉树

--- src/com/wuhunyu/solution/IsBalanced.java
+++ src/com/wuhunyu/solution/IsBalanced.java
@@ -2,9 +2,6 @@ package com.wuhunyu.solution;
 
 import com.wuhunyu.domain.tree_node.TreeNode;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 /**
  * 110. 平衡二叉树
  *
@@ -15,30 +12,13 @@ import java.util.Queue;
 
 public class IsBalanced {
 
-    public boolean isBalanced(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-            if (node.left != null) {
-                queue.offer(node.left);
-            }
-            if (node.right != null) {
-                queue.offer(node.right);
-            }
-            if (Math.abs(this.maxDepth(node.left, 0) - this.maxDepth(node.right, 0)) > 1) {
-                return false;
-            }
-        }
-        return true;
+    public boolean isBalanced(TreeNode node) {
+        return node == null || (Math.abs(this.maxDepth(node.left) - this.maxDepth(node.right)) <= 1) &&
+                this.isBalanced(node.left) && this.isBalanced(node.right);
     }
 
-    private int maxDepth(TreeNode node, int depth) {
-        return node == null ?
-                depth : Math.max(this.maxDepth(node.left, depth + 1), this.maxDepth(node.right, depth + 1));
+    private int maxDepth(TreeNode node) {
+        return node == null ? 0 : Math.max(this.maxDepth(node.left), this.maxDepth(node.right)) + 1;
     }
 
 }
